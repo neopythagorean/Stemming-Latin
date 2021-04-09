24d1b6cc5ffb9b33179c51ea299f02e421ded3df
dillonKilroy <dkilroy@emich.edu>
2021-04-08 20:06:30 -0400

    was able to get the regex set for the ruleBin and the rule. Still trying to figure out how to split the rule into the expression and the other part.

--- src/stemmer/RuleGenerator.java
+++ src/stemmer/RuleGenerator.java
@@ -3,20 +3,20 @@ package stemmer;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class RuleGenerator {
 
-	
 	public static ArrayList<String> generateCleanedSource(String sourceDir) {
 		ArrayList<String> cleanedSource = new ArrayList<String>();
-		
+
 		File dir = new File(sourceDir);
 
 		String[] stemFilesToConcatenate = dir.list();
-		
+
 		Scanner rover = null;
-		
+
 		for (int i = 0; i < stemFilesToConcatenate.length; i++) {
 			File file = new File(sourceDir + '/' + stemFilesToConcatenate[i]);
 			try {
@@ -45,23 +45,69 @@ public class RuleGenerator {
 
 					cleanedSource.add(line.trim());
 				}
-				
+
 			}
 
 			for (String s : parsedStem) {
 				System.out.println(s);
 			}
 		}
-		
+
 		return cleanedSource;
 	}
-	
+
+	public static HashMap<String, RuleBin> gernerateRuleBin(ArrayList<String> source) {
+
+		HashMap<String, RuleBin> ruleDeclaration = new HashMap<String, RuleBin>();
+
+		for (String rover : source) {
+			if (rover.matches("^[A-Za-z0-9_]+ (\\([A-Za-z0-9_]+\\) )?\\{$")) {
+				RuleBin rule = new RuleBin(rover);
+				String[] strippedRuleDeclaration = rover.split("[ ]");
+				ruleDeclaration.put(strippedRuleDeclaration[0], rule);
+//				System.out.println(strippedRuleDeclaration[0]);
+			}
+
+		}
+
+		return ruleDeclaration;
+	}
+
+	public static HashMap<String, Rule> generateRule(ArrayList<String> source) {
+
+		HashMap<String, Rule> rule = new HashMap<String, Rule>();
+
+		for (String rover : source) {
+
+			String binName = "";
+//			this is saved for the key of the hash
+			if (rover.matches("^[A-Za-z0-9_]+ (\\([A-Za-z0-9_]+\\) )?\\{$")) {
+				binName = rover + "_BIN";
+			}
+
+//			Regex that checks to see if the string is a rule
+			String expression = "\\(\\u0026 \\(m>0\\) \\(\\u007C (\\(\\*[A-Z]+\\) \\(\\*[A-Z]+\\)| \\(\\*[A-Z]+\\))\\)\\)";
+			if (rover.matches("^(\\(m>0\\) )?([A-Z]+ -> \\u03bb)$")
+					|| rover.matches(expression + " ([A-Z]+ -> [A-Z]+) (\\([A-Za-z0-9_]+\\))")) {
+//				TODO Figure out how to split the rule
+				System.out.println(rover);
+			}
+
+		}
+
+		return null;
+	}
+
 	public static ArrayList<Rule> generateRulesList(String sourceDir) {
 		ArrayList<String> source = generateCleanedSource(sourceDir);
 		ArrayList<Rule> rules = new ArrayList<Rule>();
 
+		HashMap<String, RuleBin> bins = gernerateRuleBin(source);
+
+		HashMap<String, Rule> rulesInBins = generateRule(source);
+
 		// Some logic here
-		
+
 //		String[] ruleToParse = line.split("[ ]");
 //		for (String rule : ruleToParse) {
 //				
@@ -81,9 +127,7 @@ public class RuleGenerator {
 //			else break;
 //		}
 
-		
 		return rules;
 	}
 
-	
 }
