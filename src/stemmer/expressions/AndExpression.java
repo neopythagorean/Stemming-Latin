5fab4c2cb3f9b41cf4512abf3091c1ca502ecf57
neopythagorean <flavivs.clavdivs.jvlianvs@gmail.com>
2021-04-15 20:04:42 -0400

    AND & OR Expression Constructors.

--- src/stemmer/expressions/AndExpression.java
+++ src/stemmer/expressions/AndExpression.java
@@ -11,15 +11,38 @@ public class AndExpression extends Expression {
 	
 	public AndExpression(String source) {
 		super(source);
+		this.enclosed = new ArrayList<Expression>();
 		String s = source.substring(3, source.length()-1);
-		String[] subs = s.split(" ");
-		for (String sub : subs) {
-//			try {
-//				enclosed.add(Expression.getConstructor(Expression.getExpressionType(source)).newInstance(sub));
-//			} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
-//					| InvocationTargetException e) {
-//				e.printStackTrace();
-//			}
+		int start = 0;
+		int stk = 0;
+		for (int i = 0; i < s.length()+1; i++) {
+			boolean onSpace = i == s.length();
+			if (!onSpace) {
+				char c = s.charAt(i);
+				switch(c) {
+				case ' ':
+					onSpace = true;
+					break;
+				case '(':
+					stk++;
+					break;
+				case ')':
+					stk--;
+					break;
+				}
+			}
+			
+			if (stk == 0 && onSpace) {
+				String sub = s.substring(start, i);
+				System.out.println(sub);
+				try {
+					enclosed.add(Expression.getConstructor(Expression.getExpressionType(sub)).newInstance(sub));
+				} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+						| InvocationTargetException e) {
+					e.printStackTrace();
+				}
+				start = i+1;
+			}
 		}
 	}
 	
--- src/stemmer/expressions/OrExpression.java
+++ src/stemmer/expressions/OrExpression.java
@@ -1,5 +1,6 @@
 package stemmer.expressions;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 
 import stemmer.Word;
@@ -10,6 +11,39 @@ public class OrExpression extends Expression {
 	
 	public OrExpression(String source) {
 		super(source);
+		this.enclosed = new ArrayList<Expression>();
+		String s = source.substring(3, source.length()-1);
+		int start = 0;
+		int stk = 0;
+		for (int i = 0; i < s.length()+1; i++) {
+			boolean onSpace = i == s.length();
+			if (!onSpace) {
+				char c = s.charAt(i);
+				switch(c) {
+				case ' ':
+					onSpace = true;
+					break;
+				case '(':
+					stk++;
+					break;
+				case ')':
+					stk--;
+					break;
+				}
+			}
+			
+			if (stk == 0 && onSpace) {
+				String sub = s.substring(start, i);
+				System.out.println(sub);
+				try {
+					enclosed.add(Expression.getConstructor(Expression.getExpressionType(sub)).newInstance(sub));
+				} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+						| InvocationTargetException e) {
+					e.printStackTrace();
+				}
+				start = i+1;
+			}
+		}
 	}
 
 	public boolean evaluate(Word w) {
